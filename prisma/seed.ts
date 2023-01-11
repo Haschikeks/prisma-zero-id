eae1ced177b484feee7c3c9214355ed5d9fca3e5
Max Mankel <haschikeks@users.noreplay.github.com>
2023-01-11 18:50:14 +0100

    use correct uuid; add query logging;

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,24 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [{
+    emit: 'event',
+    level: 'query',
+  }],
+});
+
+prisma.$on('query', (e) => {
+  console.log('Query: ' + e.query)
+  console.log('Params: ' + e.params)
+})
 
 async function main() {
-  const company = await prisma.company.create({ data: { id: "a" } });
-  const user = await prisma.user.create({ data: { id: "b" } });
+  const company = await prisma.company.create({
+    data: { id: "93eb7e1e-52a0-4c35-a180-47e08621450c" },
+  });
+  const user = await prisma.user.create({
+    data: { id: "1b58aaab-6176-4550-88c9-6c731a42c8db" },
+  });
   const userSettings: Prisma.UserSettingsUncheckedCreateInput = {
     userId: user.id,
     companyId: company.id,
